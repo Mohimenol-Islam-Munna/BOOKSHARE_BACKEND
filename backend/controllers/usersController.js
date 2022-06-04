ad2bc77e809827d95fbe828b241ec55b104dc1aa
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-05 05:23:40 +0600

    set routes and controller for users

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// get all user
+exports.getAllUser = (req, res) => {
+  console.log("user list");
+};
+
+// get user
+exports.getUser = (req, res) => {
+  console.log("user list");
+};
+
+// create user
+exports.createUser = (req, res) => {
+  console.log("user list");
+};
+
+// create multiple user
+exports.createMultipleUser = (req, res) => {
+  console.log("user list");
+};
+
+// update user
+exports.updateUser = (req, res) => {
+  console.log("user list");
+};
+
+// delete user
+exports.deleteUser = (req, res) => {
+  console.log("user list");
+};
---
+++ backend/routes/usersRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+// GET: users list
+router.get("/", )
+
+// GET: user
+
+// POST : create user
+
+// POST : create multiple user
+
+// PUT : update user
+
+// DELETE : delete user
+
+module.exports = router;
